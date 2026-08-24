ca52d0fcac4ea0bf27c30d4b345d6dcc5c51fa37
dkgulliver73 <dk.gulliver73@gmail.com>
2026-08-24 12:08:37 +0400

    Use active Supabase anon key

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.FOCUS_SUPABASE = {
   url: 'https://fhgolmuwinapdmiyqglv.supabase.co',
-  anonKey: 'sb_publishable_ltaNA7nnVozoSCOcZIjg'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoZ29sbXV3aW5hcGRtaXlxZ2x2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1NTI2NjgsImV4cCI6MjEwMzEyODY2OH0.WWQal2_e8ohLBASK_d3hmZXyOqOFHAQ86UbimTVBG1k'
 };
